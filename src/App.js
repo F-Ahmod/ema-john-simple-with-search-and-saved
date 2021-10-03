20a5338e3815010c74c51e20bf8606406bbf74ec
fatehahmod <ahmod.fateh@gmail.com>
2021-10-03 12:53:22 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import { BrowserRouter,Switch,Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
 import Shop from './components/Shop/Shop';
+import OrderReview from './components/OrderReview/OrderReview';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <BrowserRouter>
+      <Switch>
+        <Route path="/shop">
+          <Shop></Shop>
+
+        </Route>
+        <Route path="/inventory">
+          <Inventory></Inventory>
+
+        </Route>
+        <Route path="/OrderReview">
+          <OrderReview></OrderReview>
+        </Route>
+      </Switch>
+      </BrowserRouter>
+
+      
     </div>
   );
 }
